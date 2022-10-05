ea23c2899a9764d348487acf0229e29b20e7c6d0
Yousef Al-waer <elwaeryousef@gmail.com>
2022-10-05 23:26:08 +0200

    adding the baseic app

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
+
+var monngoURL = 'mongodb://mongo-service/database'
+
+mongoose.connect(monngoURL, { useNewUrlParser: true })
+    .then(
+        () => {
+            console.log("connected to mongo");
+        }
+    ).catch((error) => {
+        console.log("unable to connect to mongoDB : ", error);
+});
+
+
+
+app.get("/api", (req, res, next) => {
+  const data = process.env.DATA_TO_SEND || "Data Added Automatically";
+  res.json({
+    text: data,
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
