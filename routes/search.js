6dccbd757b2c2a6d9c0174333a5b27edefe2da63
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-20 04:38:56 +0200

    Search routes

---
+++ routes/search.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/search", (req, res, next) => {});
+
+router.get("/r/:sr/search", (req, res, next) => {});
+
+export default router;
