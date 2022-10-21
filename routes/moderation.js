9b40a44fab05face80d15120f951abc9b6f0565d
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-10-21 18:17:33 +0200

    GET /r/{subreddit}/about/spam

--- routes/moderation.js
+++ routes/moderation.js
@@ -105,4 +105,72 @@ const moderationRouter = express.Router();
 
 moderationRouter.get("/about/spam", (req, res, next) => {});
 
+
+/**
+ * @swagger
+ * /r/{subreddit}/about/spam:
+ *  get:
+ *   summary:
+ *    Return a listing of posts relevant to moderators in that subreddit. (This endpoint is a listing)
+ *   tags: [Moderation]
+ *   parameters:
+ *    - in: path
+ *      name: subreddit
+ *      description: name of the subreddit.
+ *      schema:
+ *       type: string
+ *      required: true
+ *    - in: query
+ *      name: after
+ *      description: fullname of a thing.
+ *      schema:
+ *       type: fullname
+ *      required: false
+ *    - in: query
+ *      name: before
+ *      description: fullname of a thing. one of after/before should be specified
+ *      schema:
+ *       type: fullname
+ *      required: false
+ *    - in: query
+ *      name: limit
+ *      description: the maximum number of items desired (default 25, maximum 100)
+ *      schema:
+ *       type: integers
+ *      required: false
+ *    - in: query
+ *      name: only
+ *      description: one of (links, comments, chat_comments)
+ *      schema:
+ *       type: enum
+ *      required: true
+ *    - in: query
+ *      name: show
+ *      description: optional parameter; if all is passed, filters such as "hide links that I have voted on" will be disabled.
+ *      schema:
+ *       type: enum
+ *      required: false
+ *    - in: query
+ *      name: show
+ *      description: optional parameter; if all is passed, filters such as "hide links that I have voted on" will be disabled.
+ *      schema:
+ *       type: enum
+ *      required: false
+ *   responses:
+ *    200:
+ *     description: Listing of posts relevant to moderators.
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/Listing'
+ *    401:
+ *     description: Unauthorized access
+ *    404:
+ *     description: Not Found
+ *    500:
+ *     description: Internal Server Error
+ */
+
+moderationRouter.get("/about/spam", (req, res, next) => {});
+
 export default moderationRouter;
