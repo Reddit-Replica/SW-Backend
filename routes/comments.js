1c2a963ea39ef0c996d172cc472180088987ffb5
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-21 21:06:02 +0200

    Comments-related routes

---
+++ routes/comments.js
@@ -0,0 +1,56 @@
+import express from "express";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Comment:
+ *       type: object
+ *       properties:
+ *         text:
+ *           type: string
+ *           description: Comment content (raw markdown text)
+ *         thing_id:
+ *           type: string
+ *           description: fullname of the thing being replied to (parent)
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  - name: Comments
+ *    description: Post comments and replies
+ */
+
+/**
+ * @swagger
+ * /comment:
+ *  post:
+ *      summary: Submit a new comment or reply to a message
+ *      tags: [Comments]
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Comment'
+ *      responses:
+ *          200:
+ *              description: Comment published
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Comment'
+ *          401:
+ *              description: Unauthorized to write a comment
+ *          500:
+ *              description: Server Error
+ *      security:
+ *       - api_key: []
+ */
+
+router.post("/comment", (req, res, next) => {});
+
+export default router;
