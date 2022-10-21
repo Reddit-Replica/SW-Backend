d106db2ef7366c79a993fbf60ade9a6a373a3b69
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-21 21:06:10 +0200

    Saved categories

---
+++ routes/categories.js
@@ -0,0 +1,47 @@
+import express from "express";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Category:
+ *       type: string
+ *       description: A category name
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  - name: Categories
+ *    description: All Categories
+ */
+
+/**
+ * @swagger
+ * /saved_categories:
+ *  get:
+ *      summary: Get a list of categories in which things are currently saved
+ *      tags: [Categories]
+ *      responses:
+ *          200:
+ *              description: Saved categories returned successfully
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                        type: array
+ *                        items:
+ *                           $ref: '#/components/schemas/Category'
+ *          404:
+ *              description: Page not found
+ *          401:
+ *              description: Unauthorized to view saved categories
+ *          500:
+ *              description: Server Error
+ *      security:
+ *       - api_key: []
+ */
+router.get("/saved_categories", (req, res, next) => {});
+
+export default router;
