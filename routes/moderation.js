414f121c170876b691702ff1d5d750c1890df705
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-10-21 18:26:48 +0200

    GET /r/:subreddit/about/edited

--- routes/moderation.js
+++ routes/moderation.js
@@ -2,8 +2,6 @@ import express from "express";
 
 const moderationRouter = express.Router();
 
-
-
 /**
  * @swagger
  *  components:
@@ -12,10 +10,10 @@ const moderationRouter = express.Router();
  *     type: object
  *     properties:
  *      id:
- *       type: string 
+ *       type: string
  *       description: this item's identifier.
  *      data:
- *       type: object 
+ *       type: object
  *       description: A custom data structure used to hold valuable information.
  *    Listing:
  *     type: object
@@ -32,17 +30,12 @@ const moderationRouter = express.Router();
  *         $ref: '#/components/schemas/ListItem'
  */
 
-
-
 /**
  * @swagger
  * tags:
  *  name: Moderation
  *  description: The Moderation endpoints API
  */
-
-
-
 
 /**
  * @swagger
@@ -104,7 +97,6 @@ const moderationRouter = express.Router();
  */
 
 moderationRouter.get("/about/spam", (req, res, next) => {});
-
 
 /**
  * @swagger
@@ -173,13 +165,12 @@ moderationRouter.get("/about/spam", (req, res, next) => {});
 
 moderationRouter.get("/r/:subreddit/about/spam", (req, res, next) => {});
 
-
 /**
  * @swagger
  * /about/edited:
  *  get:
  *   summary:
- *    Return a listing of posts relevant to moderators with things that have been edited recently.. (This endpoint is a listing)
+ *    Return a listing of required items relevant to moderators with things that have been edited recently.. (This endpoint is a listing)
  *   tags: [Moderation]
  *   parameters:
  *    - in: query
@@ -234,5 +225,72 @@ moderationRouter.get("/r/:subreddit/about/spam", (req, res, next) => {});
  */
 
 moderationRouter.get("/about/edited", (req, res, next) => {});
+
+/**
+ * @swagger
+ * /r/{subreddit}/about/edited:
+ *  get:
+ *   summary:
+ *    Return a listing of required items relevant to moderators with things that have been edited recently. in that subreddit. (This endpoint is a listing)
+ *   tags: [Moderation]
+ *   parameters:
+ *    - in: path
+ *      name: subreddit
+ *      description: name of the subreddit.
+ *      schema:
+ *       type: string
+ *      required: true
+ *    - in: query
+ *      name: after
+ *      description: fullname of a thing.
+ *      schema:
+ *       type: fullname
+ *      required: false
+ *    - in: query
+ *      name: before
+ *      description: fullname of a thing. one of after/before should be specified
+ *      schema:
+ *       type: fullname
+ *      required: false
+ *    - in: query
+ *      name: limit
+ *      description: the maximum number of items desired (default 25, maximum 100)
+ *      schema:
+ *       type: integers
+ *      required: false
+ *    - in: query
+ *      name: only
+ *      description: one of (links, comments, chat_comments)
+ *      schema:
+ *       type: enum
+ *      required: true
+ *    - in: query
+ *      name: show
+ *      description: optional parameter; if all is passed, filters such as "hide links that I have voted on" will be disabled.
+ *      schema:
+ *       type: enum
+ *      required: false
+ *    - in: query
+ *      name: show
+ *      description: optional parameter; if all is passed, filters such as "hide links that I have voted on" will be disabled.
+ *      schema:
+ *       type: enum
+ *      required: false
+ *   responses:
+ *    200:
+ *     description: Listing of required items relevant to moderators.
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/Listing'
+ *    401:
+ *     description: Unauthorized access
+ *    404:
+ *     description: Not Found
+ *    500:
+ *     description: Internal Server Error
+ */
+
+moderationRouter.get("/r/:subreddit/about/edited", (req, res, next) => {});
 
 export default moderationRouter;
