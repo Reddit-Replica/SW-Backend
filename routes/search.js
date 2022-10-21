f00cc647432769afe3b0e20f25c6fe6674e818bc
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-21 21:05:31 +0200

    Search routes

--- routes/search.js
+++ routes/search.js
@@ -2,8 +2,196 @@ import express from "express";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - kind
+ *         - sr
+ *         - title
+ *         - text
+ *       properties:
+ *         kind:
+ *           type: string
+ *           description: one of (link, self, image, video, videogif)
+ *         sr:
+ *           type: string
+ *           description: Subreddit name
+ *         text:
+ *           type: string
+ *           description: Post content as text
+ *         sendreplies:
+ *           type: boolean
+ *           description: Allow replies on post
+ *         nsfw:
+ *           type: boolean
+ *           description: Not Safe for Work
+ *         ad:
+ *           type: boolean
+ *           description: Ad or not
+ *         spoiler:
+ *           type: boolean
+ *           description: Blur the content of the post
+ *         title:
+ *           type: string
+ *           description: title of the submission. up to 300 characters long
+ *         url:
+ *           type: string
+ *           description: Post url if type is link
+ *         flair_id:
+ *           type: string
+ *           maxLength: 36
+ *           description: Flair ID
+ *         flair_text:
+ *           type: string
+ *           maxLength: 64
+ *           description: Flair text
+ *         comments:
+ *           type: number
+ *           description: Total number of comments on a post
+ *         upvotes:
+ *           type: number
+ *           description: Total number of upvotes on a post
+ *         days:
+ *           type: number
+ *           description: How many days past since the post was published
+ *         username:
+ *           type: string
+ *           description: Name of the user associated with the post
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  - name: Search
+ *    description: Search for anything in any place
+ */
+
+/**
+ * @swagger
+ * /search:
+ *  get:
+ *      summary: Search links page
+ *      tags: [Search]
+ *      parameters:
+ *          - in: query
+ *            required: true
+ *            name: q
+ *            description: Search query entered
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: type
+ *            description: one of (sr, user, comment, link)
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: sort
+ *            description: one of (hot, top, new, relevance, most comments)
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: category
+ *            description: a string no longer than 5 characters
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: t
+ *            description: one of (hour, day, week, month, year, all)
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: limit
+ *            description: the maximum number of items desired (default 25, maximum 100)
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: include_facets
+ *            description: A boolean value
+ *            schema:
+ *                  type: string
+ *      responses:
+ *          200:
+ *              description: Search results returned successfully
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                         type: array
+ *                         items:
+ *                              $ref: '#/components/schemas/Post'
+ *          404:
+ *              description: No Results found
+ *          500:
+ *              description: Server Error
+ */
 router.get("/search", (req, res, next) => {});
 
+/**
+ * @swagger
+ * /r/{sr}/search:
+ *  get:
+ *      summary: Search links page
+ *      tags: [Search]
+ *      parameters:
+ *          - in: path
+ *            required: true
+ *            name: sr
+ *            description: Subreddit name
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            required: true
+ *            name: q
+ *            description: Search query entered
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: type
+ *            description: one of (sr, user, comment, link)
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: sort
+ *            description: one of (hot, top, new, relevance, most comments)
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: category
+ *            description: a string no longer than 5 characters
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: t
+ *            description: one of (hour, day, week, month, year, all)
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: limit
+ *            description: the maximum number of items desired (default 25, maximum 100)
+ *            schema:
+ *                  type: string
+ *          - in: query
+ *            name: include_facets
+ *            description: A boolean value
+ *            schema:
+ *                  type: string
+ *      responses:
+ *          200:
+ *              description: Search results returned successfully
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                         type: array
+ *                         items:
+ *                              $ref: '#/components/schemas/Post'
+ *          404:
+ *              description: No Results found
+ *          500:
+ *              description: Server Error
+ */
 router.get("/r/:sr/search", (req, res, next) => {});
 
 export default router;
