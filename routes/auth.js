3cd8efbba487d50532d1cd1f30f2d90af2002692
BeshoyMorad <besh0morta@gmail.com>
2022-10-22 10:47:42 +0200

    Finish login, forget password and username, and reset password

--- routes/auth.js
+++ routes/auth.js
@@ -128,4 +128,149 @@ router.get("/username_available", (req, res) => {});
  */
 router.get("/email_available", (req, res) => {});
 
+/**
+ * @swagger
+ * tags:
+ *   name: Login
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Log in to the website
+ *     tags: [Login]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - username
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Username
+ *               password:
+ *                 type: string
+ *                 description: Password
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *         headers:
+ *           Authorization:
+ *             description: The jwt that will be used for authorization
+ *             schema:
+ *               type: string
+ *       400:
+ *         description: The request was invalid. You may refer to response for details around why the request was invalid
+ *         content:
+ *           application/json:
+ *             schema:
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Type of error
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/login", (req, res) => {});
+
+/**
+ * @swagger
+ * /login/forget:
+ *   post:
+ *     summary: Forget username or password
+ *     tags: [Login]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - type
+ *               - email
+ *             properties:
+ *               type:
+ *                 type: string
+ *                 description: Forget username or password
+ *                 enum:
+ *                   - username
+ *                   - password
+ *               username:
+ *                 type: string
+ *                 description: Username
+ *               email:
+ *                 type: string
+ *                 description: Email
+ *     responses:
+ *       200:
+ *         description: Email has been sent
+ *       400:
+ *         description: The request was invalid. You may refer to response for details around why the request was invalid
+ *         content:
+ *           application/json:
+ *             schema:
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Type of error
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/login/forget", (req, res) => {});
+
+/**
+ * @swagger
+ * /reset_password/{token}:
+ *   post:
+ *     summary: Reset the password
+ *     tags: [Login]
+ *     parameters:
+ *       - in: path
+ *         name: token
+ *         description: The token created by the server to reset the password
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - newPassword
+ *               - verifyPassword
+ *             properties:
+ *               newPassword:
+ *                 type: string
+ *                 description: New password
+ *               verifyPassword:
+ *                 type: string
+ *                 description: New password again to verify
+ *     responses:
+ *       200:
+ *         description: Password updated successfully
+ *         headers:
+ *           Authorization:
+ *             description: The jwt that will be used for authorization
+ *             schema:
+ *               type: string
+ *       400:
+ *         description: The request was invalid. You may refer to response for details around why the request was invalid
+ *         content:
+ *           application/json:
+ *             schema:
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Type of error
+ *       403:
+ *         description: Invalid token
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/reset_password/:token", (req, res) => {});
+
 export default router;
