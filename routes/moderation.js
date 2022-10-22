2b4f397355806333f8b5e21850d6c33a31e4941a
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-10-22 17:26:28 +0200

    edit body of /api/remove

--- routes/moderation.js
+++ routes/moderation.js
@@ -552,19 +552,21 @@ moderationRouter.post("/api/approve", (req, res, next) => {});
  *   summary:
  *    Remove  a post or comment. for spam reports, removing means that this post / comment is a spam so it is removed.
  *   tags: [Moderation]
- *   parameters:
- *    - in: body
- *      name: id
- *      description: id of a thing.
- *      required: true
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
  *      schema:
- *       type: string
- *    - in: body
- *      name: type
- *      description: type of that thing (post, comment,..).
- *      required: true
- *      schema:
- *       type: string
+ *       required:
+ *        - id
+ *        - type
+ *       properties:
+ *        id:
+ *         type: string
+ *         description: id of a thing.
+ *        type:
+ *         type: string
+ *         description: type of that thing (post, comment,..).
  *   responses:
  *    200:
  *     description: Accepted
