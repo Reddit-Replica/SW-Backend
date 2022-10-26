44cb5eb7755234f1c50821e06bce67e099930d01
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-26 18:41:53 +0200

    Add message spam and delete

--- routes/post-and-comment-actions.js
+++ routes/post-and-comment-actions.js
@@ -4,40 +4,10 @@ const router = express.Router();
 
 /**
  * @swagger
- * components:
- *   schemas:
- *     SR:
- *       type: string
- *       description: A subbreddit name
- *     ID:
- *       type: object
- *       properties:
- *          id:
- *              type: string
- *              description: id of a thing
- *          type:
- *              type: string
- *              description: one of (Post/Comment/Subreddit)
- *   securitySchemes:
- *     bearerAuth:
- *       type: http
- *       scheme: bearer
- *       bearerFormat: JWT
- */
-
-/**
- * @swagger
- * tags:
- *  - name: Post and comment actions
- *    description: User actions that are allowed on a comment or a post
- */
-
-/**
- * @swagger
- * /api/del:
+ * /del:
  *  delete:
- *      summary: Delete a Post or Comment
- *      tags: [Post and comment actions]
+ *      summary: Delete a Post, Comment or Message
+ *      tags: [Post-comment-message actions]
  *      requestBody:
  *       required: true
  *       content:
@@ -50,14 +20,24 @@ const router = express.Router();
  *                description: id of a thing created by the user
  *              type:
  *                type: string
- *                description: one of (Post/Comment)
+ *                enum:
+ *                  - post
+ *                  - comment
+ *                  - message
  *      responses:
- *          200:
- *              description: Post or comment successfully deleted
- *          401:
- *              description: Unauthorized to delete this post/comment
+ *          204:
+ *              description: Successfully deleted
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
+ *          401:
+ *              description: Unauthorized to delete this thing
  *          404:
  *              description: Item already deleted (Not Found)
  *          500:
@@ -65,14 +45,108 @@ const router = express.Router();
  *      security:
  *       - bearerAuth: []
  */
-router.delete("/api/del", (req, res, next) => {});
+router.delete("/del", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/editusertext:
+ * /spam:
+ *  post:
+ *      summary: Mark a post, comment or message as spam
+ *      tags: [Post-comment-message actions]
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              id:
+ *                type: string
+ *                description: id of a thing created by the user
+ *              type:
+ *                type: string
+ *                enum:
+ *                  - post
+ *                  - comment
+ *                  - message
+ *      responses:
+ *          200:
+ *              description: Marked as spam successfully
+ *          400:
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
+ *          404:
+ *              description: Thing not found
+ *          401:
+ *              description: Unauthorized to mark this thing as spam
+ *          409:
+ *              description: Already marked as spam
+ *          500:
+ *              description: Server Error
+ *      security:
+ *       - bearerAuth: []
+ */
+router.post("/spam", (req, res, next) => {});
+
+/**
+ * @swagger
+ * /unmarkspam:
+ *  post:
+ *      summary: Unmark a post as spam
+ *      tags: [Post-comment-message actions]
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              id:
+ *                type: string
+ *                description: id of a thing created by the user
+ *              type:
+ *                type: string
+ *                enum:
+ *                  - post
+ *                  - comment
+ *                  - message
+ *      responses:
+ *          200:
+ *              description: Unmarked as spam successfully
+ *          400:
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
+ *          404:
+ *              description: Thing not found
+ *          401:
+ *              description: Unauthorized to unmark this thing as spam
+ *          409:
+ *              description: Already unmarked as spam
+ *          500:
+ *              description: Server Error
+ *      security:
+ *       - bearerAuth: []
+ */
+router.post("/unmarkspam", (req, res, next) => {});
+
+/**
+ * @swagger
+ * /editusertext:
  *  put:
  *      summary: Edit the body text of a comment or post
- *      tags: [Post and comment actions]
+ *      tags: [Post-comment actions]
  *      requestBody:
  *       required: true
  *       content:
@@ -88,12 +162,21 @@ router.delete("/api/del", (req, res, next) => {});
  *                      description: id of the thing being edited
  *                  type:
  *                      type: string
- *                      description: one of (Post/Comment)
+ *                      enum:
+ *                          - post
+ *                          - comment
  *      responses:
  *          200:
  *              description: Post/Comment edited successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          401:
  *              description: Unauthorized to edit this post/comment
  *          404:
@@ -103,124 +186,14 @@ router.delete("/api/del", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.put("/api/editusertext", (req, res, next) => {});
+router.put("/editusertext", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/info:
- *  get:
- *      summary: Return a listing of things specified by their fullnames (Only Posts, Comments, and Subreddits)
- *      tags: [Post and comment actions]
- *      responses:
- *          200:
- *              description: Returned successfully
- *              content:
- *                  application/json:
- *                      schema:
- *                        type: object
- *                        properties:
- *                           id:
- *                              type: array
- *                              items:
- *                                  $ref: '#/components/schemas/ID'
- *                           sr_name:
- *                              type: array
- *                              items:
- *                                  $ref: '#/components/schemas/SR'
- *                           url:
- *                              type: string
- *                              description: A valid url
- *          404:
- *              description: Page not found
- *          401:
- *              description: User unauthorized to view this info
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-router.get("/api/info", (req, res, next) => {});
-
-/**
- * @swagger
- * /api/r/{sr}/info:
- *  get:
- *      summary: Return a listing of things specified by their fullnames in a subreddit
- *      tags: [Post and comment actions]
- *      parameters:
- *          - in: path
- *            name: sr
- *            schema:
- *              type: string
- *            description: Subreddit name
- *            required: true
- *      responses:
- *          200:
- *              description: Returned successfully
- *              content:
- *                  application/json:
- *                      schema:
- *                        type: object
- *                        properties:
- *                           id:
- *                              type: array
- *                              items:
- *                                  $ref: '#/components/schemas/ID'
- *                           url:
- *                              type: string
- *                              description: A valid url
- *          404:
- *              description: Page not found
- *          401:
- *              description: User unauthorized to view this info
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-router.get("/api/r/:sr/info", (req, res, next) => {});
-
-/**
- * @swagger
- * /api/lock:
- *  post:
- *      summary: Lock a post or comment (Prevents a post or new child comments from receiving replies)
- *      tags: [Post and comment actions]
- *      requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *              type: object
- *              properties:
- *                  id:
- *                    type: string
- *                    description: id of a thing
- *                  type:
- *                    type: string
- *                    description: one of (Post/Comment)
- *      responses:
- *          200:
- *              description: Post/Comment Locked successfully
- *          400:
- *              description: Bad Request
- *          401:
- *              description: User unauthorized to lock this thing
- *          409:
- *              description: Post/Comment already locked
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-router.post("/api/lock", (req, res, next) => {});
-
-/**
- * @swagger
- * /api/save:
+ * /save:
  *  post:
  *      summary: Save a post or comment
- *      tags: [Post and comment actions]
+ *      tags: [Post-comment actions]
  *      requestBody:
  *       required: true
  *       content:
@@ -233,7 +206,9 @@ router.post("/api/lock", (req, res, next) => {});
  *                    description: id of a thing
  *                  type:
  *                    type: string
- *                    description: one of (Post/Comment)
+ *                    enum:
+ *                       - post
+ *                       - comment
  *                  category:
  *                    type: string
  *                    description: A category name
@@ -241,7 +216,14 @@ router.post("/api/lock", (req, res, next) => {});
  *          200:
  *              description: Post/Comment Saved successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Post/Comment not found
  *          401:
@@ -253,14 +235,14 @@ router.post("/api/lock", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/save", (req, res, next) => {});
+router.post("/save", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/sendreplies:
+ * /sendreplies:
  *  post:
  *      summary: Enable or disable inbox replies for a Post or comment
- *      tags: [Post and comment actions]
+ *      tags: [Post-comment actions]
  *      requestBody:
  *        required: true
  *        content:
@@ -273,7 +255,9 @@ router.post("/api/save", (req, res, next) => {});
  *                    description: id of a thing created by the user
  *                  type:
  *                    type: string
- *                    description: one of (Post/Comment)
+ *                    enum:
+ *                       - post
+ *                       - comment
  *                  state:
  *                    type: boolean
  *                    description: True for enabling replies and false for disabling it
@@ -281,7 +265,14 @@ router.post("/api/save", (req, res, next) => {});
  *          200:
  *              description: Send replies settings successfully set
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Post/Comment not found
  *          401:
@@ -291,51 +282,14 @@ router.post("/api/save", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/sendreplies", (req, res, next) => {});
+router.post("/sendreplies", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/unlock:
- *  post:
- *      summary: Unlock a Post or comment
- *      tags: [Post and comment actions]
- *      requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               id:
- *                 type: string
- *                 description: id of a thing
- *               type:
- *                 type: string
- *                 description: one of (Post/Comment)
- *      responses:
- *          200:
- *              description: Post unlocked successfully
- *          400:
- *              description: Bad Request
- *          401:
- *              description: User unauthorized to unlock this thing
- *          404:
- *              description: Thing not found
- *          409:
- *              description: Thing already unlocked
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-router.post("/api/unlock", (req, res, next) => {});
-
-/**
- * @swagger
- * /api/unsave:
+ * /unsave:
  *  post:
  *      summary: Unsave a Post or comment (This removes the thing from the user's saved listings)
- *      tags: [Post and comment actions]
+ *      tags: [Post-comment actions]
  *      requestBody:
  *       required: true
  *       content:
@@ -348,12 +302,21 @@ router.post("/api/unlock", (req, res, next) => {});
  *                 description: id of a thing
  *               type:
  *                 type: string
- *                 description: one of (Post/Comment)
+ *                 enum:
+ *                     - post
+ *                     - comment
  *      responses:
  *          200:
- *              description: Post unsaved successfully
+ *              description: Unsaved successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Post/Comment not found
  *          401:
@@ -365,14 +328,14 @@ router.post("/api/unlock", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/unsave", (req, res, next) => {});
+router.post("/unsave", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/vote:
+ * /vote:
  *  post:
  *      summary: Cast a vote on a thing
- *      tags: [Post and comment actions]
+ *      tags: [Post-comment actions]
  *      requestBody:
  *       required: true
  *       content:
@@ -385,19 +348,24 @@ router.post("/api/unsave", (req, res, next) => {});
  *                 description: id of a thing
  *               type:
  *                 type: string
- *                 description: one of (Post/Comment)
+ *                 enum:
+ *                     - post
+ *                     - comment
  *               dir:
  *                 type: number
- *                 description: Vote direction.. one of (1, 0, -1)
- *               rank:
- *                 type: integer
- *                 default: 1
- *                 description: an integer greater than 1
+ *                 description: Vote direction.. one of (1, -1)
  *      responses:
  *          200:
  *              description: Vote registered successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Thing not found
  *          401:
@@ -407,6 +375,6 @@ router.post("/api/unsave", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/vote", (req, res, next) => {});
+router.post("/vote", (req, res, next) => {});
 
 export default router;
