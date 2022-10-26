9c6a2a267e5bed621f2c9b431b7ab25d305218cf
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-26 18:42:48 +0200

    Get post info

--- routes/posts.js
+++ routes/posts.js
@@ -4,77 +4,7 @@ const router = express.Router();
 
 /**
  * @swagger
- * components:
- *   schemas:
- *     Post:
- *       type: object
- *       required:
- *         - kind
- *         - sr
- *         - title
- *       properties:
- *         kind:
- *           type: string
- *           description: one of (post, self, image, video, videogif)
- *         sr:
- *           type: string
- *           description: Subreddit name
- *         text:
- *           type: string
- *           description: Post content as text
- *         sendreplies:
- *           type: boolean
- *           description: Allow replies on post
- *         nsfw:
- *           type: boolean
- *           description: Not Safe for Work
- *         ad:
- *           type: boolean
- *           description: Ad or not
- *         spoiler:
- *           type: boolean
- *           description: Blur the content of the post
- *         title:
- *           type: string
- *           max: 300
- *           description: title of the submission. up to 300 characters long
- *         url:
- *           type: string
- *           description: Post url (should be valid)
- *         flair_id:
- *           type: string
- *           max: 36
- *           description: Flair ID
- *         flair_text:
- *           type: string
- *           max: 64
- *           description: Flair text
- *         resubmit:
- *           type: boolean
- *           description: Resubmit a post
- *         share_post_id:
- *           type: string
- *           description: id of a post (given in case of sharing a post)
- *         g-recaptcha-response	:
- *           type: boolean
- *           description: Captcha result
- *   securitySchemes:
- *     bearerAuth:
- *       type: http
- *       scheme: bearer
- *       bearerFormat: JWT
- */
-
-/**
- * @swagger
- * tags:
- *  - name: Posts
- *    description: Only post-related actions
- */
-
-/**
- * @swagger
- * /api/follow_post:
+ * /follow_post:
  *  post:
  *      summary: Follow or unfollow a post.
  *      tags: [Posts]
@@ -95,7 +25,14 @@ const router = express.Router();
  *          200:
  *              description: Followed/Unfollowed post successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          401:
  *              description: User unauthorized to follow/unfollow this post
  *          404:
@@ -105,11 +42,11 @@ const router = express.Router();
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/follow_post", (req, res, next) => {});
+router.post("/follow_post", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/hide:
+ * /hide:
  *  post:
  *      summary: Hide a post (This removes it from the user's default view of subreddit listings)
  *      tags: [Posts]
@@ -127,7 +64,14 @@ router.post("/api/follow_post", (req, res, next) => {});
  *          200:
  *              description: Post hidden successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          401:
  *              description: User unauthorized to hide this post
  *          404:
@@ -139,11 +83,11 @@ router.post("/api/follow_post", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/hide", (req, res, next) => {});
+router.post("/hide", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/marknsfw:
+ * /marknsfw:
  *  post:
  *      summary: Mark a post NSFW (Not Safe For Work)
  *      tags: [Posts]
@@ -161,7 +105,14 @@ router.post("/api/hide", (req, res, next) => {});
  *          200:
  *              description: Post marked NSFW successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Post not found
  *          401:
@@ -173,11 +124,11 @@ router.post("/api/hide", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/marknsfw", (req, res, next) => {});
+router.post("/marknsfw", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/set_suggested_sort:
+ * /set_suggested_sort:
  *  post:
  *      summary: Set suggested sort for a post
  *      tags: [Posts]
@@ -198,7 +149,14 @@ router.post("/api/marknsfw", (req, res, next) => {});
  *          200:
  *              description: Suggested sort successfully set
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          401:
  *              description: User unauthorized to set suggested sort of this post
  *          404:
@@ -208,11 +166,50 @@ router.post("/api/marknsfw", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/set_suggested_sort", (req, res, next) => {});
+router.post("/set_suggested_sort", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/spoiler:
+ * /clear_suggested_sort:
+ *  post:
+ *      summary: Reset the suggested sort for a post back to default
+ *      tags: [Posts]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: string
+ *                    description: id of a post
+ *      responses:
+ *          200:
+ *              description: Suggested sort successfully cleared
+ *          400:
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
+ *          401:
+ *              description: User unauthorized to clear suggested sort of this post
+ *          404:
+ *              description: Post not found
+ *          500:
+ *              description: Server Error
+ *      security:
+ *       - bearerAuth: []
+ */
+router.post("/clear_suggested_sort", (req, res, next) => {});
+
+/**
+ * @swagger
+ * /spoiler:
  *  post:
  *      summary: Blur the content of the post and unblur when opening it
  *      tags: [Posts]
@@ -230,7 +227,14 @@ router.post("/api/set_suggested_sort", (req, res, next) => {});
  *          200:
  *              description: Spoiler set successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          401:
  *              description: User unauthorized to set post spoiler
  *          409:
@@ -242,11 +246,11 @@ router.post("/api/set_suggested_sort", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/spoiler", (req, res, next) => {});
+router.post("/spoiler", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/submit:
+ * /submit:
  *  post:
  *      summary: Submit a post to a subreddit
  *      tags: [Posts]
@@ -257,10 +261,17 @@ router.post("/api/spoiler", (req, res, next) => {});
  *           schema:
  *             $ref: '#/components/schemas/Post'
  *      responses:
- *          200:
- *              description: Post submitted
+ *          201:
+ *              description: Post submitted successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Subreddit not found
  *          401:
@@ -270,11 +281,11 @@ router.post("/api/spoiler", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/submit", (req, res, next) => {});
+router.post("/submit", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/unhide:
+ * /unhide:
  *  post:
  *      summary: Unhide a post
  *      tags: [Posts]
@@ -292,7 +303,14 @@ router.post("/api/submit", (req, res, next) => {});
  *          200:
  *              description: Post unhidden successfully
  *          400:
- *              description: Bad request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Post not found
  *          401:
@@ -304,11 +322,11 @@ router.post("/api/submit", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/unhide", (req, res, next) => {});
+router.post("/unhide", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/unmarknsfw:
+ * /unmarknsfw:
  *  post:
  *      summary: Remove the NSFW marking from a post
  *      tags: [Posts]
@@ -326,7 +344,14 @@ router.post("/api/unhide", (req, res, next) => {});
  *          200:
  *              description: NSFW unmarked successfully
  *          400:
- *              description: Bad request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Post not found
  *          401:
@@ -338,11 +363,11 @@ router.post("/api/unhide", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/unmarknsfw", (req, res, next) => {});
+router.post("/unmarknsfw", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/unspoiler:
+ * /unspoiler:
  *  post:
  *      summary: Remove ability to blur the content of the post
  *      tags: [Posts]
@@ -360,7 +385,14 @@ router.post("/api/unmarknsfw", (req, res, next) => {});
  *          200:
  *              description: Post spoiler turned off successfully
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Post not found
  *          401:
@@ -372,11 +404,11 @@ router.post("/api/unmarknsfw", (req, res, next) => {});
  *      security:
  *       - bearerAuth: []
  */
-router.post("/api/unspoiler", (req, res, next) => {});
+router.post("/unspoiler", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/insights_counts:
+ * /insights_counts:
  *  get:
  *      summary: Get the number of views on a post
  *      tags: [Posts]
@@ -387,37 +419,40 @@ router.post("/api/unspoiler", (req, res, next) => {});
  *            schema:
  *              type: string
  *            description: id of the post
- *          - in: query
- *            name: sr_name
- *            schema:
- *              type: string
- *            description: Subreddit name containing the post
  *      responses:
  *          200:
  *              description: Number of insights returned successfully
  *              content:
  *                  application/json:
  *                      schema:
- *                        type: number
- *                        description: Number of insights
+ *                        type: object
+ *                        properties:
+ *                          number_of_insights:
+ *                              type: number
+ *                              description: Number of insights on a post
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Post not found
  *          401:
  *              description: Unauthorized to view the number of insights on this post
  *          500:
  *              description: Server Error
- *      security:
- *       - bearerAuth: []
  */
-router.get("/api/insights_counts", (req, res, next) => {});
+router.get("/insights_counts", (req, res, next) => {});
 
 /**
  * @swagger
- * /api/spam:
- *  post:
- *      summary: Mark a post as spam
+ * /get_post:
+ *  get:
+ *      summary: Get details about a specific post
  *      tags: [Posts]
  *      parameters:
  *          - in: query
@@ -426,63 +461,29 @@ router.get("/api/insights_counts", (req, res, next) => {});
  *            schema:
  *              type: string
  *            description: id of the post
- *          - in: query
- *            name: sr_name
- *            schema:
- *              type: string
- *            description: Subreddit name containing the post
  *      responses:
  *          200:
- *              description: Post marked as spam successfully
+ *              description: Post info returned successfully
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                        $ref: '#/components/schemas/SearchResults'
  *          400:
- *              description: Bad Request
+ *              description: The request was invalid. You may refer to response for details around why this happened.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              error:
+ *                                  type: string
+ *                                  description: Type of error
  *          404:
  *              description: Post not found
  *          401:
- *              description: Unauthorized to mark this post as spam
- *          409:
- *              description: Post already marked as spam
+ *              description: Unauthorized to view info of this post
  *          500:
  *              description: Server Error
- *      security:
- *       - bearerAuth: []
  */
-router.post("/api/spam", (req, res, next) => {});
-
-/**
- * @swagger
- * /api/unmarkspam:
- *  post:
- *      summary: Unmark a post as spam
- *      tags: [Posts]
- *      parameters:
- *          - in: query
- *            required: true
- *            name: id
- *            schema:
- *              type: string
- *            description: id of the post
- *          - in: query
- *            name: sr_name
- *            schema:
- *              type: string
- *            description: Subreddit name containing the post
- *      responses:
- *          200:
- *              description: Post unmarked as spam successfully
- *          400:
- *              description: Bad Request
- *          404:
- *              description: Post not found
- *          401:
- *              description: Unauthorized to unmark this post as spam
- *          409:
- *              description: Post already unmarked as spam
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-router.post("/api/unmarkspam", (req, res, next) => {});
+router.get("/get_post", (req, res, next) => {});
 
 export default router;
