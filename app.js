03007fe7eab720ea9e08536daca8af6589bcddd0
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-11-01 14:03:49 +0200

    update dotenv config

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 const app = express();
 
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config();
-}
+dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 
 app.use(bodyParser.json());
 app.use(morgan("dev"));
