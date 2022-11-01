2376df280318c52fea630fca06edadba8a161f17
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-11-01 10:22:12 +0200

    Add loginRouter

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import moderationRouter from "./moderation.js";
+import loginRouter from "./login.js";
 
 const mainRouter = express.Router();
 
 mainRouter.use(moderationRouter);
+
+mainRouter.use(loginRouter);
 
 export default mainRouter;
