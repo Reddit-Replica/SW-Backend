873e7c8a2c5128e0125b41db85c50dfd450eee74
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-11-02 16:30:28 +0200

    Forget Password logic + more validators

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -15,11 +15,26 @@ const loginValidator = [
     .withMessage("Password must be at least 8 chars long"),
 ];
 
+const passwordValidator = [
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 chars long"),
+];
+
+const usernameValidator = [
+  body("username")
+    .not()
+    .isEmpty()
+    .withMessage("Username must not be empty")
+    .trim()
+    .escape(),
+];
+
 const login = async (req, res) => {
-  const email = req.body.email;
+  const username = req.body.username;
   const password = req.body.password;
   try {
-    const user = await User.findOne({ email: email });
+    const user = await User.findOne({ username: username });
     if (!user) {
       return res.status(400).send("Username not found");
     }
@@ -35,7 +50,57 @@ const login = async (req, res) => {
   }
 };
 
+const forgetPassword = async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).send("User not found");
+    }
+    if (user.username !== username) {
+      return res.status(400).send("Invalid username");
+    }
+    const token = generateJWT(user);
+    user.token = token;
+    user.expirationDate = Date.now() + 3600000;
+    await user.save();
+    transporter.sendMail({
+      to: email,
+      from: "read-it@gmail.com",
+      subject: "Password Reset",
+      html: `
+              <p>You requested a password reset</p>
+              <p>Click this <a href="http://localhost:3000/reset-password/${user.id}/${token}">link</a> to set a new password.</p>
+            `,
+    });
+    return res.status(200).send("Email has been sent");
+  } catch (err) {
+    res.status(500).send("Internal server error");
+  }
+};
+
+const forgetUsername = async (req, res) => {
+  const email = req.body.email;
+  try {
+  } catch (err) {}
+};
+
+const resetPassword = async (req, res) => {
+  const userId = req.params.id;
+  const token = req.params.token;
+  const newPassword = req.params.newPassword;
+  const verifyPassword = req.params.verifyPassword;
+  try {
+  } catch {}
+};
+
 export default {
   loginValidator,
+  usernameValidator,
+  passwordValidator,
   login,
+  forgetPassword,
+  forgetUsername,
+  resetPassword,
 };
