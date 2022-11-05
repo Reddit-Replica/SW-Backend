8dac4ca56e7f613c01463ebd3cab3e9debb8faf6
BeshoyMorad <besh0morta@gmail.com>
2022-11-05 16:32:33 +0200

    Add login router

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import signupRouter from "./signup.js";
+import loginRouter from "./login.js";
 
 // eslint-disable-next-line new-cap
 const mainRouter = express.Router();
 
 mainRouter.use(signupRouter);
+mainRouter.use(loginRouter);
 
 export default mainRouter;
