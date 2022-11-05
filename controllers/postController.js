81e66b420a653ceb0d4082abe4505403e591604f
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-11-05 04:41:24 +0200

    Pin a post and get pinned posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,13 @@
 import Post from "../models/Post";
 import User from "../models/User";
+import verifyUser from "../utils/verifyUser";
 
 // eslint-disable-next-line max-statements
 const createPost = async (req, res) => {
+  const authorizationResult = verifyUser(req);
+  if (!authorizationResult) {
+    return res.status(401).send("Token may be invalid or not found");
+  }
   const {
     kind,
     subreddit,
@@ -18,21 +23,7 @@ const createPost = async (req, res) => {
     scheduleTimeZone,
   } = req.body;
   try {
-    const authorizationHeader = req.headers.authorization;
-
-    if (!authorizationHeader) {
-      return res.status(400).json({
-        error: "Authorization Header not found!",
-      });
-    }
-    const token = authorizationHeader.split(" ")[1];
-    let userId;
-    try {
-      const decodedPayload = jwt.verify(token, process.env.TOKEN_SECRET);
-      userId = decodedPayload.userId;
-    } catch (err) {
-      return res.status(401).send("Invalid Token");
-    }
+    const userId = authorizationResult;
     const post = new Post({
       kind: kind,
       subreddit: subreddit,
@@ -59,6 +50,51 @@ const createPost = async (req, res) => {
   }
 };
 
+const pinPost = async (req, res) => {
+  const authorizationResult = verifyUser(req);
+  if (!authorizationResult) {
+    return res.status(401).send("Token may be invalid or not found");
+  }
+  const postId = req.body.id;
+  const userId = authorizationResult;
+  try {
+    const user = await User.findOne({
+      id: userId,
+    });
+    if (user.posts.find((post) => post === postId)) {
+      if (!user.pinnedPosts.find((pinnedPost) => pinnedPost === postId)) {
+        user.pinnedPosts.push(postId);
+        await user.save();
+        res.status(200).send("Post pinned successfully!");
+      } else {
+        return res.status(409).send("Post is already pinned");
+      }
+    } else {
+      return res.status(401).send("User is not the owner of this post");
+    }
+  } catch (err) {
+    res.status(500).send("Internal server error");
+  }
+};
+
+const getPinnedPosts = async (req, res) => {
+  const authorizationResult = verifyUser(req);
+  if (!authorizationResult) {
+    return res.status(401).send("Token may be invalid or not found");
+  }
+  const userId = authorizationResult;
+  try {
+    const user = await User.findOne({
+      id: userId,
+    }).populate("pinnedPosts");
+    return res.status(200).json(user.pinnedPosts);
+  } catch (err) {
+    res.status(500).send("Internal server error");
+  }
+};
+
 export default {
   createPost,
+  pinPost,
+  getPinnedPosts,
 };
