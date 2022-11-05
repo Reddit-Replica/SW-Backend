4035c3672c66da63f3af98ed20939d3a5bc23ab1
BeshoyMorad <besh0morta@gmail.com>
2022-11-05 10:53:02 +0200

    Document generate tokens file

--- utils/generateTokens.js
+++ utils/generateTokens.js
@@ -2,7 +2,13 @@ import jwt from "jsonwebtoken";
 import Token from "../models/VerifyToken.js";
 import crypto from "crypto";
 
-// Document this function [TODO]
+/**
+ * This function accepts user object and return the JWT created for that user
+ * using jwt.sign function
+ *
+ * @param {Object} user User object that contains userId, and username
+ * @returns {string} JWT created for that user
+ */
 export function generateJWT(user) {
   const token = jwt.sign(
     { userId: user.id, username: user.username },
@@ -12,6 +18,13 @@ export function generateJWT(user) {
   return token;
 }
 
+/**
+ * This function accepts user id and return a verification token
+ * that will be used later to verify email or reset password
+ *
+ * @param {string} userId User Id
+ * @returns {string} Token created for that user
+ */
 export async function generateVerifyToken(userId) {
   try {
     const token = await new Token({
