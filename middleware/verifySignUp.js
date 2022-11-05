515916db9eddecb25e99493bc47c55ae20f3a797
BeshoyMorad <besh0morta@gmail.com>
2022-11-05 11:27:17 +0200

    Document middlewares

--- middleware/verifySignUp.js
+++ middleware/verifySignUp.js
@@ -1,17 +1,28 @@
 import User from "../models/User.js";
 
-export const checkDuplicateUsernameOrEmail = async (req, res, next) => {
+/**
+ * Middleware used to check the username and email used to sign up
+ * if there was used before by another user.
+ * It searches for the username and email and if one document was found
+ * it will send status code 400 saying that the username or email is already in use
+ *
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @param {function} next Next function
+ * @returns {void}
+ */
+export async function checkDuplicateUsernameOrEmail(req, res, next) {
   try {
     // Username
     const username = await User.findOne({ username: req.body.username.trim() });
     if (username) {
-      return res.status(400).send({ error: "Username is already in use!" });
+      return res.status(400).send({ error: "Username is already in use" });
     }
 
     //Email
     const email = await User.findOne({ email: req.body.email.trim() });
     if (email) {
-      return res.status(400).send({ error: "Email is already in use!" });
+      return res.status(400).send({ error: "Email is already in use" });
     }
 
     // if everything is good then continue
@@ -19,4 +30,4 @@ export const checkDuplicateUsernameOrEmail = async (req, res, next) => {
   } catch (error) {
     res.status(500).send("Internal server error");
   }
-};
+}
