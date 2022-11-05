f7fe35939e4d53ef5f23edfb0e7c6f174ad3dc36
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-11-05 04:51:17 +0200

    Doc

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -1,5 +1,12 @@
 import jwt from "jsonwebtoken";
 
+/**
+ * This function checks for the authorization header of a request
+ * and extracts the token from it for verification with the help of jwt
+ *
+ * @param {object} req The request made
+ * @returns {string} Returns the user id if the token is verified, otherwise null
+ */
 export default function verifyUser(req) {
   const authorizationHeader = req.headers.authorization;
   if (!authorizationHeader) {
