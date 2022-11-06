5f2ae064dcb5b130dfb0b54fc3706e66ee5978fe
BeshoyMorad <besh0morta@gmail.com>
2022-11-06 21:05:04 +0200

    Comment validator and controller function

--- controllers/BcommentController.js
+++ controllers/BcommentController.js
@@ -1,7 +1,35 @@
 import { body } from "express-validator";
+import Comment from "../models/Comment.js";
 
-const createCommentValidator = [];
+const createCommentValidator = [
+  body("text").not().isEmpty().withMessage("Text can not be empty"),
+  body("parentId").not().isEmpty().withMessage("Parent Id can not be empty"),
+  body("parentType")
+    .not()
+    .isEmpty()
+    .withMessage("Parent Type can not be empty"),
+  body("level").not().isEmpty().withMessage("Level can not be empty"),
+];
 
-const createComment = (req, res) => {};
+const createComment = async (req, res) => {
+  try {
+    const { text, parentId, parentType, level } = req.body;
+    const username = req.decodedPayload.username;
+
+    const comment = new Comment({
+      parentId: parentId,
+      parentType: parentType,
+      level: level,
+      content: text,
+      ownerUsername: username,
+    });
+
+    await comment.save();
+    res.status(201).send("Comment created successfully");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 export default { createCommentValidator, createComment };
