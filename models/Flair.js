095de41648c225aa7ef910c8df190d229a476423
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-11-06 23:06:42 +0200

    Remove timestamps from Flair model

--- models/Flair.js
+++ models/Flair.js
@@ -1,58 +1,53 @@
 import mongoose from "mongoose";
 
 // eslint-disable-next-line new-cap
-const flairSchema = mongoose.Schema(
-  {
-    flairName: {
-      type: String,
-      required: true,
+const flairSchema = mongoose.Schema({
+  flairName: {
+    type: String,
+    required: true,
+  },
+  order: {
+    type: Number,
+    required: true,
+  },
+  backgroundColor: {
+    type: String,
+    default: "white",
+  },
+  textColor: {
+    type: String,
+    default: "black",
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  editedAt: {
+    type: Date,
+  },
+  deletedAt: {
+    type: Date,
+  },
+  flairSettings: {
+    modOnly: {
+      type: Boolean,
+      default: false,
     },
-    order: {
+    allowUserEdits: {
+      type: Boolean,
+      default: false,
+    },
+    flairType: {
+      type: String,
+      default: "text and emojis",
+    },
+    emojisLimit: {
       type: Number,
-      required: true,
-    },
-    backgroundColor: {
-      type: String,
-      default: "white",
-    },
-    textColor: {
-      type: String,
-      default: "black",
-    },
-    createdAt: {
-      type: Date,
-      required: true,
-      default: Date.now(),
-    },
-    editedAt: {
-      type: Date,
-    },
-    deletedAt: {
-      type: Date,
-    },
-    flairSettings: {
-      modOnly: {
-        type: Boolean,
-        default: false,
-      },
-      allowUserEdits: {
-        type: Boolean,
-        default: false,
-      },
-      flairType: {
-        type: String,
-        default: "text and emojis",
-      },
-      emojisLimit: {
-        type: Number,
-        default: 10,
-      },
+      default: 10,
     },
   },
-  {
-    timestamps: true,
-  }
-);
+});
 
 const Flair = mongoose.model("Flair", flairSchema);
 
