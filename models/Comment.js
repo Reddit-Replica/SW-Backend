9d358b7bcfdd3e8fe18d3e1cabf1716675a1987a
BeshoyMorad <besh0morta@gmail.com>
2022-11-06 19:33:22 +0200

    Add comment model

---
+++ models/Comment.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+
+// eslint-disable-next-line new-cap
+const commentSchema = mongoose.Schema({
+  postId: {
+    type: Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+  parentId: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment",
+    required: true,
+  },
+  parentType: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: Number,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  numberOfVotes: {
+    type: Number,
+    required: true,
+  },
+  deletedAt: {
+    type: Date,
+  },
+  ownerUsername: {
+    type: String,
+    required: true,
+  },
+  markedSpam: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  editAt: {
+    type: Date,
+  },
+  children: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
