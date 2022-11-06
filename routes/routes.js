8bea2802920ea96c163aea6e1c10cd0efbca0a09
BeshoyMorad <besh0morta@gmail.com>
2022-11-06 19:33:56 +0200

    Add comment endpoint to the routers

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import moderationRouter from "./moderation.js";
 import signupRouter from "./signup.js";
 import loginRouter from "./login.js";
+import commentsRouter from "./comments.js";
 
 // eslint-disable-next-line new-cap
 const mainRouter = express.Router();
 
 mainRouter.use(signupRouter);
 mainRouter.use(loginRouter);
+mainRouter.use(commentsRouter);
 
 export default mainRouter;
