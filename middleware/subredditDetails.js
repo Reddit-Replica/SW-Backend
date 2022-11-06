b2ed9a3f54d397e6b2589a0449080e285d584939
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-11-06 20:05:01 +0200

    Add functional documentation

--- middleware/subredditDetails.js
+++ middleware/subredditDetails.js
@@ -20,6 +20,17 @@ const createSubreddit = async (req, _res, next) => {
   next();
 };
 
+/**
+ * A middleware used to make sure that the provided subreddit name exists
+ * If that subreddit exists it adds it to the request object to make the next middleware access it
+ * It it doesn't exist then it return a response with status code 404 and error message
+ *
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @param {function} next Next function
+ * @returns {void}
+ */
+
 const checkSubreddit = async (req, res, next) => {
   const subredditName = req.params.subreddit;
   const subbredditObject = await Subreddit.findOne({
--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,5 +1,13 @@
 import jwt from "jsonwebtoken";
 
+/**
+ * A middleware used to verify the token and send back status code 401 with error message if the token is invalid or not provided
+ *
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @param {function} next Next function
+ * @returns {void}
+ */
 const verifyAuthToken = (req, res, next) => {
   try {
     const authorizationHeader = req.headers.authorization;
