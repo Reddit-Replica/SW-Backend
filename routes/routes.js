8b4b04c2f05d03ba48f4b820dc31100727f8fbc5
BeshoyMorad <besh0morta@gmail.com>
2022-11-07 08:36:48 +0200

    Add post, comment, and message actions router

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ import moderationRouter from "./moderation.js";
 import signupRouter from "./signup.js";
 import loginRouter from "./login.js";
 import commentsRouter from "./comments.js";
+import postCommentMessageActionsRouter from "./poComMesActions.js";
 
 // eslint-disable-next-line new-cap
 const mainRouter = express.Router();
@@ -10,5 +11,6 @@ const mainRouter = express.Router();
 mainRouter.use(signupRouter);
 mainRouter.use(loginRouter);
 mainRouter.use(commentsRouter);
+mainRouter.use(postCommentMessageActionsRouter);
 
 export default mainRouter;
