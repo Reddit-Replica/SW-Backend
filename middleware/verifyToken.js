da750b22820ca7849ccbdc2f860fb9b32ea2efe3
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-11-07 14:21:59 +0200

    verify the user is moderator in that subreddit

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -13,8 +13,9 @@ const verifyAuthToken = (req, res, next) => {
     const authorizationHeader = req.headers.authorization;
     // console.log(authorizationHeader);
     const token = authorizationHeader.split(" ")[1];
-    jwt.verify(token, process.env.TOKEN_SECRET);
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
 
+    req.payload = payload;
     next();
   } catch (err) {
     res.status(401).json({
@@ -23,6 +24,33 @@ const verifyAuthToken = (req, res, next) => {
   }
 };
 
+/**
+ * A middleware used to verify that the user requesting the subreddits rules is a moderator in that subreddit
+ * If not it returns status code 401 Unauthorized Access
+ * Else it pass the request to the next middleware
+ *
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @param {function} next Next function
+ * @returns {void}
+ */
+
+const verifyAuthTokenModerator = (req, res, next) => {
+  const moderators = req.subreddit.moderators;
+  const payload = req.payload;
+  const moderator = moderators.find(
+    (mod) => mod.userID.toString() === payload.userId
+  );
+  if (!moderator) {
+    res.status(401).json({
+      error: "Unauthorized Access",
+    });
+  } else {
+    next();
+  }
+};
+
 export default {
   verifyAuthToken,
+  verifyAuthTokenModerator,
 };
