191395343b011007592f107ee1628b1c3cb58619
BeshoyMorad <besh0morta@gmail.com>
2022-11-08 17:10:47 +0200

    Add edit posts and comments router

--- routes/post-and-comment-actions.js
+++ routes/post-and-comment-actions.js
@@ -147,53 +147,6 @@ router.post("/unmark-spam");
 
 /**
  * @swagger
- * /edit-user-text:
- *  put:
- *      summary: Edit the body text of a comment or post
- *      tags: [Post-comment actions]
- *      requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *              type: object
- *              properties:
- *                  text:
- *                      type: string
- *                      description: New text entered
- *                  id:
- *                      type: string
- *                      description: id of the thing being edited
- *                  type:
- *                      type: string
- *                      enum:
- *                          - post
- *                          - comment
- *      responses:
- *          200:
- *              description: Post/Comment edited successfully
- *          400:
- *              description: The request was invalid. You may refer to response for details around why this happened.
- *              content:
- *                  application/json:
- *                      schema:
- *                          properties:
- *                              error:
- *                                  type: string
- *                                  description: Type of error
- *          401:
- *              description: Unauthorized to edit this post/comment
- *          404:
- *              description: Content requested for editing is unavailable
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-router.put("/edit-user-text");
-
-/**
- * @swagger
  * /save:
  *  post:
  *      summary: Save a post or comment
