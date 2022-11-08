9f0fde7a088302697b2b12e35d709b9edff8b995
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-11-08 06:01:44 +0200

    VerifyUser test

---
+++ tests/utils/verifyUserSpec.js
@@ -0,0 +1,44 @@
+import verifyUser from "../../utils/verifyUser.js";
+import { generateJWT } from "../../utils/generateTokens.js";
+import User from "../../models/User.js";
+import Token from "./../../models/VerifyToken.js";
+import supertest from "supertest";
+import app from "../../app.js";
+supertest(app);
+
+fdescribe("Testing verifying a user is logged in", () => {
+  afterAll(async () => {
+    await User.deleteMany({});
+    await Token.deleteMany({});
+  });
+
+  it("should have verifyUser method", () => {
+    expect(verifyUser).toBeDefined();
+  });
+
+  it("check if verifyUser verifies a valid jwt", () => {
+    const user = {
+      id: "mongodbId",
+      username: "Hamdy",
+    };
+    const token = generateJWT(user);
+    const req = {
+      body: {
+        token: token,
+      },
+    };
+    const decodedPayload = verifyUser(req);
+    expect(decodedPayload.userId).toEqual("mongodbId");
+    expect(decodedPayload.username).toEqual("Hamdy");
+  });
+
+  it("Send an invalid token to verifyUser", () => {
+    const req = {
+      body: {
+        token: "invalidToken",
+      },
+    };
+    const decodedPayload = verifyUser(req);
+    expect(decodedPayload).toBeNull();
+  });
+});
