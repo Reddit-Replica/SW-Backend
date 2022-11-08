b4fe4d8373488e60d5cf3eeb4f5fa34d1306bfc7
BeshoyMorad <besh0morta@gmail.com>
2022-11-08 17:15:04 +0200

    Moved some routes to other files

--- routes/posts.js
+++ routes/posts.js
@@ -90,47 +90,6 @@ postRouter.post("/hide");
 
 /**
  * @swagger
- * /mark-nsfw:
- *  patch:
- *      summary: Mark a post NSFW (Not Safe For Work)
- *      tags: [Posts]
- *      requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *              type: object
- *              properties:
- *                  id:
- *                    type: string
- *                    description: id of a post
- *      responses:
- *          200:
- *              description: Post marked NSFW successfully
- *          400:
- *              description: The request was invalid. You may refer to response for details around why this happened.
- *              content:
- *                  application/json:
- *                      schema:
- *                          properties:
- *                              error:
- *                                  type: string
- *                                  description: Type of error
- *          404:
- *              description: Post not found
- *          401:
- *              description: User unauthorized to mark post as NSFW
- *          409:
- *              description: Post already marked NSFW
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-postRouter.patch("/mark-nsfw");
-
-/**
- * @swagger
  * /set-suggested-sort:
  *  post:
  *      summary: Set suggested sort for a post comments
@@ -212,47 +171,6 @@ postRouter.post("/clear-suggested-sort");
 
 /**
  * @swagger
- * /mark-spoiler:
- *  patch:
- *      summary: Blur the content of the post and unblur when opening it
- *      tags: [Posts]
- *      requestBody:
- *        required: true
- *        content:
- *          application/json:
- *             schema:
- *                type: object
- *                properties:
- *                  id:
- *                    type: string
- *                    description: id of a post
- *      responses:
- *          200:
- *              description: Spoiler set successfully
- *          400:
- *              description: The request was invalid. You may refer to response for details around why this happened.
- *              content:
- *                  application/json:
- *                      schema:
- *                          properties:
- *                              error:
- *                                  type: string
- *                                  description: Type of error
- *          401:
- *              description: User unauthorized to set post spoiler
- *          409:
- *              description: Post content already blurred
- *          404:
- *              description: Post not found
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-postRouter.patch("/mark-spoiler");
-
-/**
- * @swagger
  * /submit:
  *  post:
  *      summary: Submit or share a post to a subreddit
@@ -326,88 +244,6 @@ postRouter.post("/submit", postController.createPost);
  *       - bearerAuth: []
  */
 postRouter.post("/unhide");
-
-/**
- * @swagger
- * /unmark-nsfw:
- *  patch:
- *      summary: Remove the NSFW marking from a post
- *      tags: [Posts]
- *      requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               id:
- *                 type: string
- *                 description: id of a post
- *      responses:
- *          200:
- *              description: NSFW unmarked successfully
- *          400:
- *              description: The request was invalid. You may refer to response for details around why this happened.
- *              content:
- *                  application/json:
- *                      schema:
- *                          properties:
- *                              error:
- *                                  type: string
- *                                  description: Type of error
- *          404:
- *              description: Post not found
- *          401:
- *              description: User unauthorized to remove nsfw marking
- *          409:
- *              description: NSFW mark already removed
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-postRouter.patch("/unmark-nsfw");
-
-/**
- * @swagger
- * /unmark-spoiler:
- *  patch:
- *      summary: Remove ability to blur the content of the post
- *      tags: [Posts]
- *      requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               id:
- *                 type: string
- *                 description: id of a post
- *      responses:
- *          200:
- *              description: Post spoiler turned off successfully
- *          400:
- *              description: The request was invalid. You may refer to response for details around why this happened.
- *              content:
- *                  application/json:
- *                      schema:
- *                          properties:
- *                              error:
- *                                  type: string
- *                                  description: Type of error
- *          404:
- *              description: Post not found
- *          401:
- *              description: User unauthorized to turn off spoiler
- *          409:
- *              description: Post spoiler already turned off
- *          500:
- *              description: Server Error
- *      security:
- *       - bearerAuth: []
- */
-postRouter.patch("/unmark-spoiler");
 
 /**
  * @swagger
