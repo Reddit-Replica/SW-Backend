9fd2899f0bff5ff285f84265890ea87f61730c74
BeshoyMorad <besh0morta@gmail.com>
2022-11-11 22:48:31 +0200

    Finish block user endpoint

--- controllers/BuserController.js
+++ controllers/BuserController.js
@@ -1,4 +1,5 @@
 import { body } from "express-validator";
+import User from "./../models/User.js";
 
 const blockUserValidator = [
   body("username")
@@ -20,8 +21,37 @@ const followUserValidator = [
   body("follow").not().isEmpty().withMessage("Follow flag can not be empty"),
 ];
 
+// eslint-disable-next-line max-statements
 const blockUser = async (req, res) => {
   try {
+    const { username, block } = req.body;
+
+    const userToBlock = await User.findOne({ username: username });
+    if (!userToBlock) {
+      return res.status(404).json("Didn't find a user with that username");
+    }
+
+    const { userId } = req.payload;
+    const user = await User.findById(userId);
+
+    // get the index of the id of the user to be blocked if he was blocked before
+    const index = user.blockedUsers.findIndex(
+      (elem) => elem.toString() === userToBlock._id.toString()
+    );
+
+    if (block) {
+      if (index === -1) {
+        user.blockedUsers.push(userToBlock._id);
+        await user.save();
+      }
+      res.status(200).json("User blocked successfully");
+    } else {
+      if (index !== -1) {
+        user.blockedUsers.splice(index, 1);
+        await user.save();
+      }
+      res.status(200).json("User unblocked successfully");
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json("Internal Server Error");
