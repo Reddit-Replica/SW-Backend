40972c6ebc2e126dd85dc8e72cdc1bb848c46a9e
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-11-16 22:49:32 +0200

    Calling service functions in the controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,17 +1,17 @@
 import Category from "../models/Category.js";
+import {
+  getSortedCategories,
+  insertCategoriesIfNotExists,
+} from "../services/categories.js";
 
 const getAllCategories = async (req, res, next) => {
-  const categories = await Category.find({})?.sort({
-    randomIndex: 1,
-  });
-  return res.status(200).json(
-    categories.map((category) => {
-      return {
-        id: category.id,
-        name: category.name,
-      };
-    })
-  );
+  try {
+    await insertCategoriesIfNotExists();
+    const categories = await getSortedCategories();
+    return res.status(200).json(categories);
+  } catch (err) {
+    return res.status(500).json("Internal server error");
+  }
 };
 
 export default {
