2e1b1e21bc0aa8bb983bf63d5b7f7d56fbfa5d14
BeshoyMorad <besh0morta@gmail.com>
2022-11-16 16:47:07 +0200

    Document the user services file

--- services/userServices.js
+++ services/userServices.js
@@ -1,5 +1,12 @@
 import User from "../models/User.js";
 
+/**
+ * Service to search for a user object with his username and return it in the req
+ *
+ * @param {Object} req Request
+ * @param {String} username Username that we want find
+ * @returns {void}
+ */
 export async function searchForUserService(req, username) {
   const user = await User.findOne({ username: username });
   if (!user) {
@@ -10,6 +17,12 @@ export async function searchForUserService(req, username) {
   req.foundUser = user;
 }
 
+/**
+ * Service to get the user object from the jwt that was sent in the header
+ *
+ * @param {Object} req Request
+ * @returns {void}
+ */
 export async function getUserFromJWTService(req) {
   const { userId } = req.payload;
   const user = await User.findById(userId);
@@ -21,6 +34,16 @@ export async function getUserFromJWTService(req) {
   req.user = user;
 }
 
+/**
+ * Service used to block or unblock a certain user after checking that
+ * they are different users.
+ * If the operation was block operation, then we check that the user was not blocked before first.
+ *
+ * @param {Object} user User Object
+ * @param {Object} userToBlock User that we want to block or unblock
+ * @param {Boolean} block Flag to know if the operation is block or unblock
+ * @returns {Object} Response to the request containing [statusCode, message]
+ */
 export async function blockUserService(user, userToBlock, block) {
   if (user._id.toString() === userToBlock._id.toString()) {
     let error = new Error("User can not block himself");
@@ -54,6 +77,16 @@ export async function blockUserService(user, userToBlock, block) {
   }
 }
 
+/**
+ * Service used to follow or unfollow a certain user after checking that
+ * they are different users.
+ * If the operation was followed operation, then we check that the user was not followeded before first.
+ *
+ * @param {Object} user User Object
+ * @param {Object} userToFollow User that we want to follow or unfollow
+ * @param {Boolean} follow Flag to know if the operation is follow or unfollow
+ * @returns {Object} Response to the request containing [statusCode, message]
+ */
 export async function followUserService(user, userToFollow, follow) {
   if (user._id.toString() === userToFollow._id.toString()) {
     let error = new Error("User can not himself himself");
