52e748d6ccc23116752f8e1a9f98cb8f5bd300e4
BeshoyMorad <besh0morta@gmail.com>
2022-11-18 22:16:48 +0200

    Add user id to post when he comment on that post

--- controllers/BcommentController.js
+++ controllers/BcommentController.js
@@ -2,6 +2,8 @@ import { body } from "express-validator";
 import Comment from "../models/Comment.js";
 import User from "./../models/User.js";
 import Community from "./../models/Community.js";
+import Post from "./../models/Post.js";
+import mongoose from "mongoose";
 
 const createCommentValidator = [
   body("text").not().isEmpty().withMessage("Text can not be empty"),
@@ -23,6 +25,21 @@ const createComment = async (req, res) => {
     const { text, parentId, parentType, level, subredditName, haveSubreddit } =
       req.body;
     const { username, userId } = req.payload;
+
+    // if the parent is a post get it
+    let post = {};
+    if (parentType === "post") {
+      if (!mongoose.Types.ObjectId.isValid(parentId)) {
+        return res.status(400).json({ error: "Invalid parent id" });
+      }
+      post = await Post.findById(parentId);
+      if (!post || post.deletedAt) {
+        return res
+          .status(400)
+          .json({ error: "Can not find post with that id" });
+      }
+    }
+
     const commentObject = {
       parentId: parentId,
       parentType: parentType,
@@ -34,7 +51,10 @@ const createComment = async (req, res) => {
 
     // check if the subreddit exists
     if (haveSubreddit) {
-      const subreddit = await Community.findOne({ title: subredditName });
+      const subreddit = await Community.findOne({
+        title: subredditName,
+        deletedAt: null,
+      });
       if (!subreddit) {
         return res
           .status(400)
@@ -53,6 +73,14 @@ const createComment = async (req, res) => {
     const user = await User.findById(userId);
     user.comments.push(comment._id);
     await user.save();
+
+    const index = post.usersCommented.findIndex(
+      (elem) => elem.toString() === userId
+    );
+    if (index === -1) {
+      post.usersCommented.push(user._id);
+      await post.save();
+    }
 
     res.status(201).json("Comment created successfully");
   } catch (error) {
