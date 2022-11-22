019281879d25f1af328fc53f1803587e960772e4
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-11-22 22:23:34 +0200

    Edit cors

--- app.js
+++ app.js
@@ -52,7 +52,10 @@ const port = process.env.PORT || 3000;
 
 app.use((_req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
