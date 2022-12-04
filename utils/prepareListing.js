c9e411460454e1ed3fb49f09ecf6a23846686a19
BeshoyMorad <besh0morta@gmail.com>
2022-12-04 16:18:10 +0200

    Add comment prepare listing and final listing functions

--- utils/prepareListing.js
+++ utils/prepareListing.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import Post from "./../models/Post.js";
+import Comment from "./../models/Comment.js";
 
 /**
  * Function used to prepare the sorting object that will be used by mongoose to sort the results
@@ -84,7 +85,7 @@ function preparePostTime(listingTime, sort) {
  * @param {Number} listingLimit Time interval that we want to get the post in
  * @returns {Object} Object that will be used by mongoose to limit the results
  */
-function preparePostLimit(listingLimit) {
+function prepareLimit(listingLimit) {
   let result = {};
   if (!listingLimit && listingLimit !== 0) {
     result = 25;
@@ -119,7 +120,7 @@ async function preparePostBeforeAfter(before, after, sort, sortingType) {
     if (mongoose.Types.ObjectId.isValid(before)) {
       // get the wanted value that we will split from
       const post = await Post.findById(before);
-      if (!post) {
+      if (!post || post.deletedAt) {
         return null;
       }
 
@@ -141,7 +142,7 @@ async function preparePostBeforeAfter(before, after, sort, sortingType) {
     if (mongoose.Types.ObjectId.isValid(after)) {
       // get the wanted value that we will split from
       const post = await Post.findById(after);
-      if (!post) {
+      if (!post || post.deletedAt) {
         return null;
       }
 
@@ -173,7 +174,6 @@ async function preparePostBeforeAfter(before, after, sort, sortingType) {
  * @param {Object} listingParams Listing parameters sent in the request query [sort, time, before, after, limit]
  * @returns {Object} Result object containing the final results after listing [sort, time, listing, limit]
  */
-// eslint-disable-next-line max-statements
 export async function prepareListingPosts(listingParams) {
   let result = {},
     sortingType = {};
@@ -187,7 +187,7 @@ export async function prepareListingPosts(listingParams) {
   result.time = preparePostTime(listingParams.time, listingParams.sort);
 
   // prepare the limit
-  result.limit = preparePostLimit(listingParams.limit);
+  result.limit = prepareLimit(listingParams.limit);
 
   // check if after or before
   result.listing = await preparePostBeforeAfter(
@@ -204,7 +204,7 @@ export async function prepareListingPosts(listingParams) {
  * Function to create the exact condition that will be used by mongoose directly to list posts.
  * Used to map every listing parameter to the exact query that mongoose will use later
  *
- * @param {Object} listingParams Result of prepareListingParameters function
+ * @param {Object} listingParams Result of prepareListingPosts function
  * @returns {Object} The final results that will be used by mongoose to list posts
  */
 export async function postListing(listingParams) {
@@ -220,6 +220,162 @@ export async function postListing(listingParams) {
   } else if (listingParams.time) {
     result.find = { createdAt: listingParams.time, deletedAt: null };
   } else if (listingParams.listing) {
+    result.find = { deletedAt: null };
+    result.find[listingParams.listing.type] = listingParams.listing.value;
+  } else {
+    result.find = { deletedAt: null };
+  }
+
+  result.sort = listingParams.sort;
+  result.limit = listingParams.limit;
+
+  return result;
+}
+
+/**
+ * Function used to prepare the sorting object that will be used by mongoose to sort the results
+ *
+ * @param {String} listingSort Sorting type used to sort the wanted comments
+ * @returns {Array} Array with two elements [sort, sortingType]
+ */
+function prepareCommentSort(listingSort) {
+  let result = {},
+    sortingType = {};
+
+  if (!listingSort) {
+    // best
+    listingSort = "best";
+    result = { numberOfVotes: -1 };
+    sortingType = { type: "numberOfVotes" };
+  } else {
+    switch (listingSort) {
+      case "new":
+        result = { createdAt: -1 };
+        sortingType = { type: "createdAt" };
+        break;
+      case "old":
+        result = { createdAt: 1 };
+        sortingType = { type: "createdAt" };
+        break;
+      case "top":
+        result = null;
+        break;
+      default:
+        result = { numberOfVotes: -1 };
+        sortingType = { type: "numberOfVotes" };
+        break;
+    }
+  }
+  return [result, sortingType];
+}
+
+/**
+ * Function used to prepare the anchor point of the slice that will be used by mongoose to match the results
+ *
+ * @param {String} before Id of the comment that we want to get the comments before it
+ * @param {String} after Id of the comment that we want to get the comments after it
+ * @param {String} sort Sorting type used to sort the wanted comments
+ * @param {String} sortingType Sorting parameter in the comment model
+ * @returns {Object} Object that will be used by mongoose to match the results
+ */
+// eslint-disable-next-line max-statements
+async function prepareCommentBeforeAfter(before, after, sort, sortingType) {
+  let result = {};
+  if (!after && !before) {
+    return null;
+  } else if (!after && before) {
+    if (mongoose.Types.ObjectId.isValid(before)) {
+      // get the wanted value that we will split from
+      const comment = await Comment.findById(before);
+      if (!comment || comment.deletedAt) {
+        return null;
+      }
+
+      if (sort) {
+        result = {
+          type: sortingType.type,
+          value: { $gt: comment[sortingType.type] },
+        };
+      } else {
+        result = {
+          type: "_id",
+          value: { $lt: before },
+        };
+      }
+    } else {
+      return null;
+    }
+  } else if (after && !before) {
+    if (mongoose.Types.ObjectId.isValid(after)) {
+      // get the wanted value that we will split from
+      const comment = await Comment.findById(after);
+      if (!comment || comment.deletedAt) {
+        return null;
+      }
+
+      if (sort) {
+        result = {
+          type: sortingType.type,
+          value: { $lt: comment[sortingType.type] },
+        };
+      } else {
+        result = {
+          type: "_id",
+          value: { $gt: after },
+        };
+      }
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+  return result;
+}
+
+/**
+ * Function to prepare the listing parameters and set the appropriate condition that will be used with mongoose later.
+ * Check the sort algorithm, limit of the result, and the anchor point of the slice
+ * to get the previous or the next things
+ *
+ * @param {Object} listingParams Listing parameters sent in the request query [sort, before, after, limit]
+ * @returns {Object} Result object containing the final results after listing [sort, time, listing, limit]
+ */
+export async function prepareListingComments(listingParams) {
+  let result = {},
+    sortingType = {};
+
+  //prepare the sorting
+  const sortingResult = prepareCommentSort(listingParams.sort);
+  result.sort = sortingResult[0];
+  sortingType = sortingResult[1];
+
+  // prepare the limit
+  result.limit = prepareLimit(listingParams.limit);
+
+  // check if after or before
+  result.listing = await prepareCommentBeforeAfter(
+    listingParams.before,
+    listingParams.after,
+    result.sort,
+    sortingType
+  );
+
+  return result;
+}
+
+/**
+ * Function to create the exact condition that will be used by mongoose directly to list comments.
+ * Used to map every listing parameter to the exact query that mongoose will use later
+ *
+ * @param {Object} listingParams Result of prepareListingComments function
+ * @returns {Object} The final results that will be used by mongoose to list comments
+ */
+export async function commentListing(listingParams) {
+  let result = {};
+  listingParams = await prepareListingComments(listingParams);
+
+  if (listingParams.listing) {
     result.find = { deletedAt: null };
     result.find[listingParams.listing.type] = listingParams.listing.value;
   } else {
