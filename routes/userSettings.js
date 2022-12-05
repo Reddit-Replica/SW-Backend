896a4b69d3a0aed923fc1eef9df90764f0905754
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-12-05 18:17:56 +0200

    Disconnect google/facebook account

--- routes/userSettings.js
+++ routes/userSettings.js
@@ -163,11 +163,15 @@ router.patch("/account-settings");
  *         application/json:
  *           schema:
  *             required:
+ *               - password
  *               - accessToken
  *             properties:
  *               accessToken:
  *                 type: string
  *                 description: Access token from the response of google or facebook
+ *               password:
+ *                 type: string
+ *                 description: Password entered for verification
  *     responses:
  *       200:
  *         description: Connected successfully
@@ -188,6 +192,54 @@ router.patch("/account-settings");
  *       - bearerAuth: []
  */
 router.post("/connect/:type");
+
+/**
+ * @swagger
+ * /disconnect/{type}:
+ *   post:
+ *     summary: Disconnect google or facebook account
+ *     tags: [User settings]
+ *     parameters:
+ *       - in: path
+ *         name: type
+ *         description: Type of disconnect
+ *         required: true
+ *         schema:
+ *           type: string
+ *           enum:
+ *             - google
+ *             - facebook
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - password
+ *             properties:
+ *               password:
+ *                 type: string
+ *                 description: Password entered for verification
+ *     responses:
+ *       200:
+ *         description: Disconnected successfully
+ *       400:
+ *         description: The request was invalid. You may refer to response for details around why the request was invalid
+ *         content:
+ *           application/json:
+ *             schema:
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Type of error
+ *       401:
+ *         description: Access Denied
+ *       500:
+ *         description: Internal server error
+ *     security:
+ *       - bearerAuth: []
+ */
+router.post("/disconnect/:type");
 
 /**
  * @swagger
