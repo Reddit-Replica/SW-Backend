c8224bf8d2fbbc3c3b468f1ae926a709ad5cbc92
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-12-07 00:19:35 +0200

    add docs

--- services/commentActionsServices.js
+++ services/commentActionsServices.js
@@ -1,6 +1,11 @@
 import User from "../models/User.js";
 import Comment from "../models/Comment.js";
 
+/**
+ * A function used to validate the comment and check if that comment exists and throws error if the comment is not found and returns the comment if it exists
+ * @param {ObjectId} commentId the id of the comment
+ * @returns {Comment} the neededComment
+ */
 async function validateExistingComment(commentId) {
   const neededComment = await Comment.findById(commentId);
   if (!neededComment || neededComment.deletedAt) {
@@ -11,6 +16,12 @@ async function validateExistingComment(commentId) {
   return neededComment;
 }
 
+/**
+ * A function used to add the comment to the user followed comments
+ * @param {ObjectId} userId the id of the user
+ * @param {ObjectId} commentId the id of the comment
+ * @returns {Object} the neededComment and user to make the next step easier
+ */
 export async function addToUserFollowedComments(userId, commentId) {
   const neededUser = await User.findById(userId);
   const neededComment = await validateExistingComment(commentId);
@@ -27,6 +38,12 @@ export async function addToUserFollowedComments(userId, commentId) {
   return { comment: neededComment, user: neededUser };
 }
 
+/**
+ * A function used to add the user to the comment following users
+ * @param {User} user that specific user
+ * @param {Comment} comment that specific comment
+ * @returns {void}
+ */
 export async function addToCommentFollowedUsers(user, comment) {
   comment.followingUsers.push({
     username: user.username,
