852164763c657c1416ddcee654a96b4613994f41
BeshoyMorad <besh0morta@gmail.com>
2022-12-08 19:17:41 +0200

    Ban user services documentation

--- services/subredditActionsServices.js
+++ services/subredditActionsServices.js
@@ -1,5 +1,11 @@
 import Subreddit from "../models/Community.js";
 
+/**
+ * Function used to check that a subreddit with that name exists and return its object
+ *
+ * @param {String} subredditName Name of the subreddit to return
+ * @returns {Object} Subreddit object that was found
+ */
 export async function getSubredditService(subredditName) {
   const subreddit = await Subreddit.findOne({
     title: subredditName,
@@ -13,6 +19,16 @@ export async function getSubredditService(subredditName) {
   return subreddit;
 }
 
+/**
+ * Function used to ban a user from a certain subreddit. It checks if [moderator] is a moderator of
+ * the wanted subreddit. It also checks if the user was banned before in the same subreddit.
+ *
+ * @param {Object} moderator Moderator object of the subreddit
+ * @param {Object} userToBan User object that we want to ban
+ * @param {Object} subreddit Subreddit object
+ * @param {Object} data Request body containing [banPeriod, reasonForBan, modNote, noteInclude]
+ * @returns The response to that request containing [statusCode, data]
+ */
 export async function banUserService(moderator, userToBan, subreddit, data) {
   // check if user is moderator in the subreddit
   const index = subreddit.moderators.findIndex(
