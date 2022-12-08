178a33444c41c2082655378387c30728fd5cb661
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-12-08 20:28:24 +0200

    Remove dumb edit

--- controllers/HpostController.js
+++ controllers/HpostController.js
@@ -23,7 +23,6 @@ const submit = async (req, res) => {
   try {
     user.posts.push(post.id);
     user.upvotedPosts.push(post.id);
-    user.commentedPosts.push(post.id);
     post.numberOfUpvotes = 1;
     post.numberOfVotes = 1;
     user.upVotes += 1;
