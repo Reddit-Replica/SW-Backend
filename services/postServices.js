188d300ba0b3b79d6d4a72f4f6af55e0a8f36aa4
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-12-10 19:53:39 +0200

    adding docs

--- services/postServices.js
+++ services/postServices.js
@@ -1,6 +1,12 @@
 import Post from "../models/Post.js";
 import Subreddit from "../models/Community.js";
 
+/**
+ * A function used to check if the owner user is trying to edit the post and throws an error if another user is trying to edit that post
+ * @param {ObjectID} postId Request object
+ * @param {ObjectID} userId Request object
+ * @returns {neededPost} the needed post to edit is all checks succeeded to help the next service
+ */
 export async function checkSameUserEditing(postId, userId) {
   const neededPost = await Post.findById(postId);
   if (!neededPost || neededPost.deletedAt) {
@@ -21,6 +27,12 @@ export async function checkSameUserEditing(postId, userId) {
   return neededPost;
 }
 
+/**
+ * A function used to edit the post and add the post tothe subreddit edited posts if the post belongs to a subreddit
+ * @param {post} post Request object
+ * @param {string} postContent Request object
+ * @returns {void} the needed post to edit is all checks succeeded to help the next service
+ */
 export async function editPostService(post, postContent) {
   const subreddit = post.subredditName;
   const subbredditObject = await Subreddit.findOne({
