efae05635a996e71a2852a349295c541a586090c
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-12-17 00:36:17 +0200

    UPDATE doc

--- routes/notification.js
+++ routes/notification.js
@@ -52,7 +52,7 @@ const notificationRouter = express.Router();
  *                           type: string
  *                           description:  Only one of after/before should be specified. The id of last item in the listing to use as the anchor point of the slice and get the next things.
  *                          unreadCount:
- *                           type: Number
+ *                           type: number
  *                           description: The number of unread notification returned
  *                          children:
  *                            type: array
