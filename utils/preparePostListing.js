ce9a123f2b682391a5d2084dccd5384401360862
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-12-17 16:43:48 +0200

    Refactor fixing the sort

--- utils/preparePostListing.js
+++ utils/preparePostListing.js
@@ -35,6 +35,10 @@ function preparePostSort(listingSort) {
         result = { createdAt: 1 };
         sortingType = { type: "createdAt" };
         break;
+      case "trending":
+        result = { numberOfViews: -1 };
+        sortingType = { type: "numberOfViews" };
+        break;
       default:
         result = { createdAt: -1 };
         sortingType = { type: "createdAt" };
@@ -110,7 +114,7 @@ async function preparePostBeforeAfter(before, after, sort, sortingType) {
       }
 
       if (sort) {
-        let value = { $gt: post[sortingType.type] };
+        let value = { $gte: post[sortingType.type] };
         // eslint-disable-next-line max-depth
         if (sort.createdAt === 1) {
           value = { $lt: post[sortingType.type] };
@@ -137,7 +141,7 @@ async function preparePostBeforeAfter(before, after, sort, sortingType) {
       }
 
       if (sort) {
-        let value = { $lt: post[sortingType.type] };
+        let value = { $lte: post[sortingType.type] };
         // eslint-disable-next-line max-depth
         if (sort.createdAt === 1) {
           value = { $gt: post[sortingType.type] };
