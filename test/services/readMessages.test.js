09489fd4f74857e0dd57066220822dfb95efe3ea
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-12-18 04:15:53 +0200

    Enhance coverage

--- test/services/readMessages.test.js
+++ test/services/readMessages.test.js
@@ -12,6 +12,7 @@ import Comment from "./../../models/Comment";
 import Message from "./../../models/Message.js";
 import Mention from "./../../models/Mention.js";
 import PostReplies from "./../../models/PostReplies.js";
+import mongoose from "mongoose";
 
 // eslint-disable-next-line max-statements
 describe("Testing Read Messages Service functions", () => {
@@ -98,7 +99,7 @@ describe("Testing Read Messages Service functions", () => {
     expect(readReceivedMessages).toBeDefined();
   });
 
-  it("Test Read receivedMessages", async () => {
+  it("Test receivedMessages", async () => {
     receiverUser.receivedMessages = [message.id];
     await receiverUser.save();
     await readReceivedMessages(receiverUser.id);
@@ -108,11 +109,21 @@ describe("Testing Read Messages Service functions", () => {
     expect(receiverUser.receivedMessages[0].isRead).toBeTruthy();
   });
 
+  it("Test receivedMessages with an invalid user ID", async () => {
+    try {
+      const invalidId = mongoose.Types.ObjectId.generate(10);
+      await readReceivedMessages(invalidId);
+    } catch (err) {
+      expect(err.statusCode).toEqual(401);
+      expect(err.message).toEqual("User not found");
+    }
+  });
+
   it("Should have readUsernameMentions defined", () => {
     expect(readUsernameMentions).toBeDefined();
   });
 
-  it("Test Read readUsernameMentions", async () => {
+  it("Test readUsernameMentions", async () => {
     receiverUser.usernameMentions = [mention.id];
     await receiverUser.save();
     await readUsernameMentions(receiverUser.id);
@@ -120,6 +131,16 @@ describe("Testing Read Messages Service functions", () => {
       username: receiverUser.username,
     }).populate("usernameMentions");
     expect(receiverUser.usernameMentions[0].isRead).toBeTruthy();
+  });
+
+  it("Test readUsernameMentions with an invalid user ID", async () => {
+    try {
+      const invalidId = mongoose.Types.ObjectId.generate(10);
+      await readUsernameMentions(invalidId);
+    } catch (err) {
+      expect(err.statusCode).toEqual(401);
+      expect(err.message).toEqual("User not found");
+    }
   });
 
   it("Should have readPostReplies defined", () => {
@@ -134,5 +155,15 @@ describe("Testing Read Messages Service functions", () => {
       username: receiverUser.username,
     }).populate("postReplies");
     expect(receiverUser.postReplies[0].isRead).toBeTruthy();
+  });
+
+  it("Test readPostReplies with an invalid user ID", async () => {
+    try {
+      const invalidId = mongoose.Types.ObjectId.generate(10);
+      await readPostReplies(invalidId);
+    } catch (err) {
+      expect(err.statusCode).toEqual(401);
+      expect(err.message).toEqual("User not found");
+    }
   });
 });
