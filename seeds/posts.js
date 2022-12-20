46b545eef992771eb71634923e01fc33fd188543
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-12-20 20:55:53 +0200

    Posts still not inserted

--- seeds/posts.js
+++ seeds/posts.js
@@ -1,12 +1,16 @@
 export const posts = [];
+let users, subreddits;
 
 import { faker } from "@faker-js/faker";
 import User from "../models/User.js";
 import Subreddit from "../models/Community.js";
 
+(async function () {
+  users = await User.find({});
+  subreddits = await Subreddit.find({});
+})();
+
 async function createRandomPost() {
-  const users = await User.find({});
-  const subreddits = await Subreddit.find({});
   const userIndex = Math.round(Math.random(0, users.length));
   const subredditIndex = Math.round(Math.random(0, subreddits.length));
   return {
@@ -21,6 +25,6 @@ async function createRandomPost() {
   };
 }
 
-Array.from({ length: 3 }).forEach(async () => {
+Array.from({ length: 20 }).forEach(async () => {
   posts.push(await createRandomPost());
 });
--- seeds/seeder.js
+++ seeds/seeder.js
@@ -5,14 +5,13 @@ import Category from "../models/Category.js";
 import Subreddit from "../models/Community.js";
 import Post from "../models/Post.js";
 dotenv.config();
-import { users } from "./users.js";
+import { faker } from "@faker-js/faker";
 import { categories } from "./categories.js";
-import { subreddits } from "./subreddits.js";
-import { posts } from "./posts.js";
 const DB_URL = process.env.MONGO_URL_SEED.trim();
 
 export let userIds;
 export let subredditsId;
+let users = [];
 
 async function connectDatabase() {
   try {
@@ -39,10 +38,58 @@ async function seedCategories() {
   await Category.insertMany(categories);
 }
 
+let subreddits = [];
+
+function createRandomSubreddit() {
+  const userIndex = Math.round(Math.random(0, 10));
+  const randomDate = faker.date.past();
+  const name = faker.internet.userName();
+  return {
+    title: name,
+    viewName: name,
+    category: categories[Math.round(Math.random(0, 5))].name,
+    type: "Public",
+    nsfw: "false",
+    owner: {
+      userID: users[userIndex]._id,
+      username: users[userIndex].username,
+    },
+    dateOfCreation: randomDate,
+    joinedUsers: [{ joinDate: randomDate, userID: users[userIndex]._id }],
+    approvedUsers: [
+      { dateOfApprove: randomDate, userID: users[userIndex]._id },
+    ],
+  };
+}
+
+Array.from({ length: 5 }).forEach(() => {
+  subreddits.push(createRandomSubreddit());
+});
+
 async function seedSubreddits() {
   await Subreddit.deleteMany();
   await Subreddit.insertMany(subreddits);
 }
+
+async function createRandomPost() {
+  const userIndex = Math.round(Math.random(0, 10));
+  const subredditIndex = Math.round(Math.random(0, 5));
+  return {
+    kind: "link",
+    ownerUsername: users[userIndex].username,
+    ownerId: users[userIndex]._id,
+    subredditName: subreddits[subredditIndex].title,
+    subredditId: subreddits[subredditIndex]._id,
+    title: faker.internet.userName(),
+    link: faker.internet.url(),
+    createdAt: faker.date.past(),
+  };
+}
+let posts = [];
+
+Array.from({ length: 20 }).forEach(async () => {
+  posts.push(await createRandomPost());
+});
 
 async function seedPosts() {
   await Post.deleteMany();
@@ -55,8 +102,10 @@ async function seedPosts() {
   await connectDatabase();
 
   await seedUsers();
+  users = await User.find({});
   await seedCategories();
   await seedSubreddits();
+  subreddits = await Subreddit.find({});
   await seedPosts();
 
   closeDatabaseConnection();
