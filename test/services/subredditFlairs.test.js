95cf8564ce64269e51c33bf7bbf174ea182c9618
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-12-23 13:49:55 +0200

    Flairs tests

--- test/services/subredditFlairs.test.js
+++ test/services/subredditFlairs.test.js
@@ -5,6 +5,12 @@ import {
   createFlair,
   validateId,
   editFlair,
+  checkFlair,
+  deleteFlair,
+  prepareFlairDetails,
+  prepareFlairsSettings,
+  prepareFlairs,
+  validateFlairSettingsBody,
 } from "../../services/subredditFlairs.js";
 import Subreddit from "../../models/Community.js";
 import Flair from "../../models/Flair.js";
@@ -13,6 +19,13 @@ import { jest } from "@jest/globals";
 import { connectDatabase, closeDatabaseConnection } from "../database.js";
 
 describe("Testing subreddit flairs services", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(() => {
+    closeDatabaseConnection();
+  });
   describe("testing validateCreateOrEditFlair method", () => {
     it("validateCreateOrEditFlair method should exist", () => {
       expect(validateCreateOrEditFlair).toBeDefined();
@@ -173,7 +186,6 @@ describe("Testing subreddit flairs services", () => {
 
   describe("Testing create flair function", () => {
     it("Create a valid flair", async () => {
-      await connectDatabase();
       const subredditObject = await new Subreddit({
         title: "title",
         viewName: "title",
@@ -208,7 +220,6 @@ describe("Testing subreddit flairs services", () => {
       expect(saveFunction).toHaveBeenCalled();
       await Subreddit.deleteMany({});
       await Flair.deleteMany({});
-      await closeDatabaseConnection();
     });
   });
 
@@ -246,6 +257,188 @@ describe("Testing subreddit flairs services", () => {
       const flairObj = {};
       await editFlair(flairObj, flair);
       expect(saveFunction).toHaveBeenCalled();
+    });
+  });
+  describe("Testing checkFlair ", () => {
+    it("searching existing flair", async () => {
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+      }).save();
+      const createdAt = Date.now();
+      const flairObj = await new Flair({
+        flairName: "Test flair",
+        subreddit: subredditObject._id,
+        flairOrder: 2,
+        createdAt: createdAt,
+      }).save();
+      subredditObject.flairs.push(flairObj._id);
+      await subredditObject.save();
+      expect(
+        await checkFlair(flairObj._id.toString(), subredditObject)
+      ).toMatchObject({
+        flairName: "Test flair",
+      });
+
+      await Flair.deleteMany({});
+    });
+    it("searching deleted flair", async () => {
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+      }).save();
+      const createdAt = Date.now();
+      const flairObj = await new Flair({
+        flairName: "Test flair",
+        subreddit: subredditObject._id,
+        flairOrder: 2,
+        createdAt: createdAt,
+      }).save();
+      flairObj.deletedAt = Date.now();
+      subredditObject.flairs.push(flairObj._id);
+      await subredditObject.save();
+      await flairObj.save();
+      await expect(
+        checkFlair(flairObj._id.toString(), subredditObject)
+      ).rejects.toThrow("Flair not found");
+      await Flair.deleteMany({});
+    });
+  });
+
+  describe("Testing deleteFlair", () => {
+    it("Deleting a falir", async () => {
+      const saveFunction1 = jest.fn();
+      const saveFunction2 = jest.fn();
+      const subreddit = {
+        save: saveFunction1,
+        numberOfFlairs: 2,
+      };
+      const flair = {
+        save: saveFunction2,
+      };
+
+      await deleteFlair(flair, subreddit);
+      expect(subreddit.numberOfFlairs).toEqual(1);
+      expect(saveFunction1).toHaveBeenCalled();
+      expect(saveFunction2).toHaveBeenCalled();
+    });
+  });
+
+  describe("Testing prepareFlairDetails", () => {
+    it("Details of flair without text color or backgound color", () => {
+      const createdAt = Date.now();
+      const flair = {
+        flairName: "Test flair",
+        flairOrder: 2,
+      };
+      expect(prepareFlairDetails(flair)).toMatchObject({
+        flairName: "Test flair",
+        flairOrder: 2,
+      });
+    });
+    it("Details of flair with text color and backgound color", () => {
+      const createdAt = Date.now();
+      const flair = {
+        flairName: "Test flair",
+        flairOrder: 2,
+        textColor: "red",
+        backgroundColor: "blue",
+      };
+      expect(prepareFlairDetails(flair)).toMatchObject({
+        flairName: "Test flair",
+        flairOrder: 2,
+        textColor: "red",
+        backgroundColor: "blue",
+      });
+    });
+  });
+
+  describe("Testing prepareFlairsSettings", () => {
+    it("subreddit falir settings", () => {
+      const subreddit = {
+        flairSettings: {
+          enablePostFlairInThisCommunity: true,
+          allowUsersToAssignTheirOwn: false,
+        },
+      };
+      expect(prepareFlairsSettings(subreddit)).toEqual(
+        expect.objectContaining({
+          enablePostFlairs: true,
+          allowUsers: false,
+        })
+      );
+    });
+  });
+
+  describe("Testing prepareFlairs", () => {
+    it("flairs", async () => {
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+      }).save();
+      const createdAt = Date.now();
+      const flair1 = await new Flair({
+        flairName: "Test flair",
+        subreddit: subredditObject._id,
+        flairOrder: 2,
+        createdAt: createdAt,
+      }).save();
+      flair1.deletedAt = Date.now();
+      const flair2 = await new Flair({
+        flairName: "Test flair2",
+        subreddit: subredditObject._id,
+        flairOrder: 3,
+        createdAt: createdAt,
+      }).save();
+      flair1.deletedAt = Date.now();
+      subredditObject.flairs.push(flair1._id);
+      subredditObject.flairs.push(flair2._id);
+      await subredditObject.save();
+      await flair1.save();
+      const flairs = await prepareFlairs(subredditObject);
+      expect(flairs.length).toBe(1);
+    });
+  });
+
+  describe("Testing validateFlairSettingsBody", () => {
+    it("invalid flair settings", () => {
+      const req = {
+        body: {},
+      };
+      expect(() => {
+        validateFlairSettingsBody(req);
+      }).toThrow("Bad request");
+    });
+    it("invalid flair settings", () => {
+      const req = {
+        body: { allowUsers: true, enablePostFlairs: false },
+      };
+      expect(() => {
+        validateFlairSettingsBody(req);
+      }).toThrow("Bad request");
+    });
+    it("invalid flair settings", () => {
+      const req = {
+        body: { allowUsers: true, enablePostFlairs: true },
+      };
+      expect(() => {
+        validateFlairSettingsBody(req);
+      }).not.toThrowError();
     });
   });
 });
