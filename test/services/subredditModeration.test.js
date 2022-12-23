c62c3777b98e217ccd7d44e35869330809992983
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-12-23 18:56:36 +0200

    getSubredditApproved

--- test/services/subredditModeration.test.js
+++ test/services/subredditModeration.test.js
@@ -1,6 +1,7 @@
 import {
   getSubredditModerators,
   getSubredditInvitedModerators,
+  getSubredditApproved,
 } from "../../services/subredditModerationServices.js";
 import { connectDatabase, closeDatabaseConnection } from "../database.js";
 import User from "../../models/User.js";
@@ -1102,6 +1103,547 @@ describe("Testing subredditModerationServices", () => {
         subredditObject
       );
       expect(invitedModerators.children.length).toBe(2);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+  });
+  describe("Testing getSubredditApproved", () => {
+    it("Setting after and before", async () => {
+      await expect(
+        getSubredditApproved(2, true, true, { populate: () => {} })
+      ).rejects.toThrow("Can't set before and after");
+    });
+    it("Didn't set neither after nor before", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        2,
+        false,
+        false,
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(1);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Didn't set neither after nor before", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        0,
+        false,
+        false,
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(1);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Set before", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        2,
+        user._id.toString(),
+        false,
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(0);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Set after", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        0,
+        false,
+        user._id.toString(),
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(0);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Invalid modetaor id after", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user2 = await new User({
+        username: "zeyad2",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      await expect(
+        getSubredditApproved(0, false, user2._id.toString(), subredditObject)
+      ).rejects.toThrow("invalid approved id");
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Invalid modetaor id before", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user2 = await new User({
+        username: "zeyad2",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      await expect(
+        getSubredditApproved(0, user2._id.toString(), false, subredditObject)
+      ).rejects.toThrow("invalid approved id");
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Set after", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user2 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user3 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user2._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user3._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        1,
+        false,
+        user._id.toString(),
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(1);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Set nothing", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user2 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user3 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user2._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user3._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        1,
+        false,
+        false,
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(1);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Set after", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user2 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user3 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user4 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user2._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user3._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user4._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        1,
+        false,
+        user._id.toString(),
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(1);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Set before", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user2 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user3 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user4 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user2._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user3._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user4._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        1,
+        user._id.toString(),
+        false,
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(0);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Set before", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user2 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user3 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user4 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user2._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user3._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user4._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        1,
+        user2._id.toString(),
+        false,
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(1);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Set before", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user2 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user3 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user2._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user3._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        2,
+        user3._id.toString(),
+        false,
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(2);
+      await Subreddit.deleteMany({});
+      await User.deleteMany({});
+    });
+    it("Set before", async () => {
+      const user = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user2 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user3 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const user4 = await new User({
+        username: "zeyad",
+        createdAt: Date.now(),
+      }).save();
+      const subredditObject = await new Subreddit({
+        title: "title",
+        viewName: "title",
+        category: "Sports",
+        type: "Public",
+        owner: {
+          username: "zeyad",
+        },
+        approvedUsers: [
+          {
+            userID: user._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user2._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user3._id,
+            dateOfApprove: Date.now(),
+          },
+          {
+            userID: user4._id,
+            dateOfApprove: Date.now(),
+          },
+        ],
+      }).save();
+      const approvedUsers = await getSubredditApproved(
+        2,
+        user4._id.toString(),
+        false,
+        subredditObject
+      );
+      expect(approvedUsers.children.length).toBe(2);
       await Subreddit.deleteMany({});
       await User.deleteMany({});
     });
